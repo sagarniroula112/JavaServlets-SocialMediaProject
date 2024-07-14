68e00af12a24d623dc9f95ebb6283c081e39a43f
Sagar Niroula <sagarniroula112@gmail.com>
2024-07-14 10:43:03 +0545

    DB class done!

--- social_media/src/main/java/com/db/DB.java
+++ social_media/src/main/java/com/db/DB.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 
 public class DB {
 	public static Connection dbConnect() {
+		// DB class for preventing code redundancy
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			return DriverManager.getConnection("jdbc:mysql://localhost:3306/socialmediadb", "root", "hello");
