3aec1601b0b0d395bc5a68aa8b9e812f6841efe8
Sagar Niroula <sagarniroula112@gmail.com>
2024-07-24 21:45:47 +0545

    DB class file updated!

--- social_media/src/main/java/com/db/DB.java
+++ social_media/src/main/java/com/db/DB.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 
 public class DB {
 	public static Connection dbConnect() {
+		// use password that was saved before
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			return DriverManager.getConnection("jdbc:mysql://localhost:3306/socialmediadb", "root", "hello");
